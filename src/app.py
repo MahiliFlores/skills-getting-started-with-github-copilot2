68ffd017cef1cc4ab6f42e2eb235d544acc3eb4b
MahiliFlores <mahiliflores@outlook.com>
2026-05-07 16:39:01 +0000

    Add new extracurricular activity

--- src/app.py
+++ src/app.py
@@ -38,7 +38,13 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
-    }
+    },
+    "Art Club": {
+    "description": "Learn drawing and painting techniques.",
+    "schedule": "Wednesdays, 3:30 PM - 5:00 PM",
+    "availability": 15,
+    "participants": []
+}
 }
 
 
